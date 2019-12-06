5d92fe01a1e1e953d328b1a72f4882340e89e0ef
icol2006 <icol2006@hotmail.com>
2019-12-06 09:29:01 -0600

    v05

--- Aplication/Web/App_Start/BundleConfig.cs
+++ Aplication/Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                         "~/Scripts/jquery.min.js"));
+                        // "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -20,11 +21,43 @@ namespace Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
+                      "~/assets/plugins/jquery/jquery.min.js",
+                      "~/assets/plugins/popper/popper.min.js",
+                      "~/assets/plugins/jquery-blockui/jquery.blockui.min.js",
+                      "~/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                      "~/assets/plugins/bootstrap/js/bootstrap.min.js",
+                      "~/assets/plugins/datatables/jquery.dataTables.min.js",
+                      "~/assets/plugins/datatables/plugins/bootstrap/dataTables.bootstrap4.min.js",
+                      "~/assets/js/pages/table/table_data.js",
+                      "~/assets/js/app.js",
+                      "~/assets/js/layout.js",
+                      "~/assets/js/theme-color.js",
+                      "~/assets/plugins/material/material.min.js",
+                      "~/assets/js/pages/material_select/getmdl-select.js",
+                      "~/assets/plugins/material-datetimepicker/moment-with-locales.min.js",
+                      "~/assets/plugins/material-datetimepicker/bootstrap-material-datetimepicker.js",
+                      "~/assets/plugins/material-datetimepicker/datetimepicker.js",
+                      "~/assets/plugins/dropzone/dropzone.js",
+                      "~/assets/plugins/dropzone/dropzone-call.js",
+                      "~/assets/js/pages/ui/animations.js"
+                      ));
+            
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/fonts/material-design-icons/material-icon.css",
+                      "~/fonts/font-awesome/css/font-awesome.min.css",
+                      "~/assets/plugins/bootstrap/css/bootstrap.min.css",
+                       "~/assets/plugins/material/material.min.css",
+                      "~/assets/css/material_style.css",
+                      "~/assets/plugins/datatables/plugins/bootstrap/dataTables.bootstrap4.min.css",
+                     
+                      "~/assets/css/pages/animate_page.css",
+                      "~/assets/css/style.css",
+                      "~/assets/css/plugins.min.css",
+                      "~/assets/css/responsive.css",
+                      "~/assets/css/theme-color.css",
+                      "~/assets/plugins/dropzone/dropzone.css",
+                      "~/assets/plugins/material-datetimepicker/bootstrap-material-datetimepicker.css"                      
+                      ));
         }
     }
 }
